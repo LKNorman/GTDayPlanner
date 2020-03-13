b1a3a465dd1a1f481559277fce4b885e221b49b1
LKNorman <lknorman30@gmail.com>
2020-03-13 17:52:08 -0400

    added input areas and basic callbacks for color changes

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-  
   // test constant
   const test = false;
 
   // get times from moment
-  const now = moment().format('MMMM Do YYYY');
+  const now = moment().format("MMMM Do YYYY");
 
-  let nowHour24 = moment().format('H');
-  let nowHour12 = moment().format('h');
+  let nowHour24 = moment().format("H");
+  let nowHour12 = moment().format("h");
 
-  let $dateHeading = $('#dateH');
+  let $dateHeading = $("#dateH");
   $dateHeading.text(now);
-  
+
   // using font awesome icon https://fontawesome.com/license
-  const saveIcon = "./images/save-regular.svg"; 
+  const saveIcon = "./images/save-regular.svg";
 
   // Parsing the JSON string to an object
   let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
@@ -26,42 +25,76 @@ $(document).ready(function() {
   }
 
   // set variable referencing planner element
-  let $plannerDiv = $('#plannerContainer');
+  let $plannerDiv = $("#plannerContainer");
   $plannerDiv.empty();
 
   // build calendar by row for fix set of hours
   for (let hour = 9; hour <= 17; hour++) {
     let index = hour - 9;
-    
+
     // build row components
-    let $rowDiv = $('<div>');
-    $rowDiv.addClass('row');
-    $rowDiv.addClass('plannerRow');
-    $rowDiv.attr('hour-index',hour);
-  
+    let $rowDiv = $("<div>");
+    $rowDiv.addClass("row");
+    $rowDiv.addClass("plannerRow");
+    $rowDiv.attr("hour-index", hour);
+
     // Time box portion of row
-    let $col2TimeDiv = $('<div>');
-    $col2TimeDiv.addClass('col-md-2');
-    const $timeBoxSpn = $('<span>');
-    $timeBoxSpn.attr('class','timeBox');
+    let $col2TimeDiv = $("<div>");
+    $col2TimeDiv.addClass("col-md-2");
+    const $timeBoxSpn = $("<span>");
+    $timeBoxSpn.attr("class", "timeBox");
     //format hours
     let displayHour = 0;
     let ampm = "";
-    if (hour > 12) { 
+    if (hour > 12) {
       displayHour = hour - 12;
       ampm = "pm";
     } else {
       displayHour = hour;
       ampm = "am";
     }
-    
+
     // display time
     $timeBoxSpn.text(`${displayHour} ${ampm}`);
     $rowDiv.append($col2TimeDiv);
     $col2TimeDiv.append($timeBoxSpn);
 
-    // START building input portion of row
-    // build row components
-    let $dailyPlanSpn = $('<input>');
+    // input portion of row
+    let $dailyPlanSpn = $("<input>");
+
+    $dailyPlanSpn.attr("id", `input-${index}`);
+    $dailyPlanSpn.attr("hour-index", index);
+    $dailyPlanSpn.attr("type", "text");
+    $dailyPlanSpn.attr("class", "dailyPlan");
+
+    // access index for hour
+    $dailyPlanSpn.val(planTextArr[index]);
+
+    // width control
+    let $col9IptDiv = $("<div>");
+    $col9IptDiv.addClass("col-md-9");
+
+    $rowDiv.append($col9IptDiv);
+    $col9IptDiv.append($dailyPlanSpn);
+
+    // save portion of row
+    let $col1SaveDiv = $("<div>");
+    $col1SaveDiv.addClass("col-md-1");
+
+    let $saveBtn = $("<i>");
+    $saveBtn.attr("id", `saveid-${index}`);
+    $saveBtn.attr("save-id", index);
+    $saveBtn.attr("class", "far fa-save saveIcon");
+
+    // more width control
+    $rowDiv.append($col1SaveDiv);
+    $col1SaveDiv.append($saveBtn);
+
+    // set row color based on time
+    updateRowColor($rowDiv, hour);
+
+    // add row to planner 
+    $plannerDiv.append($rowDiv);
+  }
 
 });
